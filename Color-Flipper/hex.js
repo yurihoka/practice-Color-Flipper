334575ae4125e9dd2590fda8d388882713736579
Yurika Hokama <yurihoka@gmail.com>
2024-03-03 23:39:31 -0800

    add func for change bkground

---
+++ Color-Flipper/hex.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+const getRandomNumber = function () {
+  return Math.floor(Math.random() * hex.length);
+};
